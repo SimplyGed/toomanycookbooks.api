88a6c74326c84e208590c59598ffe0bfc2d014c0
Ged Moretta <ged.moretta@patientzero.com.au>
2022-04-25 08:36:53 +1000

    add version to configuration settings

---
+++ toomanycookbooks.api/Configuration/GlobalConfiguration.cs
@@ -0,0 +1,6 @@
+public class GlobalConfiguration
+{
+    public static string Name => "Global";
+
+    public string? Version { get; set; }
+}
--- toomanycookbooks.api/Program.cs
+++ toomanycookbooks.api/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+builder.Services.Configure<GlobalConfiguration>(builder.Configuration.GetSection(GlobalConfiguration.Name));
 
 var app = builder.Build();
 
