4cb8c2af4f575cf941aa4412e85cd0cf6f007ce7
Ged Moretta <ged.moretta@patientzero.com.au>
2022-04-24 14:23:20 +1000

    add basic models for book and recipe

---
+++ toomanycookbooks.api.data/Models/Book.cs
@@ -0,0 +1,25 @@
+namespace TooManyCookbooks.Api.Data.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+    }
+
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+
+        public ICollection<Ingredient>? Ingredients { get; set; }
+
+        public virtual Book? Book { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
